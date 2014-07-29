7cfc2631837ff1f5af45dcd8dfa282abd93f9186
David Leon Gil <coruus@gmail.com>
2014-07-29 00:23:32 -0400

    Reduced incorrectness.

--- crypto/openpgp-cfb/ocfb.py
+++
@@ -1,9 +0,0 @@
-"""OpenPGP-CFB mode test vector generator.
-
-This generates test vectors of two kinds:
-    - RFC 4880 compliant test-vectors
-    - Compatibility test-vectors
-
-OpenPGP implementations should accept the compatibility
-test-vectors for decryption.
-"""
